c586657e3191ba389640e590d26da411d302cd37
Chen <tianyang.chen@hpe.com>
2018-02-17 11:35:33 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <h1 className="App-title">Welcome to Code</h1>
+        </header>
+       
+        <div tyle="color: 'red'" >
+        <h1>DevOps</h1>
+        <h1>Microservices</h1>
+        <h1>Data</h1>
+        <h1>Digital Transformation</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//<img src={logo} className="App-logo" alt="logo" />
+
+
+//  <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
