1da9a319091725f75ec171982bd14c10e44683ab
david chen <skysunchen@live.com>
2018-04-09 16:01:09 +0800

    first

---
+++ src/App.bk.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+//import './App.css';
+import MenuExampleBasic from './menu';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Code</h1>
+          
+          <MenuExampleBasic />
+        </header>
+
+
+
+        <div tyle="color: 'red'" >
+
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+
+
+
+        <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+
+
+
+
+        <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+
+
+
+        <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+
+
+        <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+
+        <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+       <h1>DevOps</h1>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//<img src={logo} className="App-logo" alt="logo" />
+
+
+//  <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+
+//http://jsfiddle.net/LEqjm/484/
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+
+//https://css-tricks.com/on-target/
+
+//https://codepen.io/team/css-tricks/pen/BzQNPm
+
+//https://www.w3schools.com/Jsref/tryit.asp?filename=tryjsref_element_scrollintoview2
+
+//https://stackoverflow.com/questions/22808040/fading-out-text-on-overflow-with-css-if-the-text-is-bigger-than-allowed
+
+
+
+// this is what i want
+//Animating the `content` Property
+//https://css-tricks.com/animating-the-content-property/
+
+//https://github.com/Semantic-Org/Semantic-UI-React
